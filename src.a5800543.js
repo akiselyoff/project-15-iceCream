1a0eefa54d7cb789c7a9288da15423fd7b52f8ae
akiselyoff <a.kiselyoff@gmail.com>
2021-12-02 21:58:24 +0000

    Deploying to gh-pages from @ akiselyoff/project-15-iceCream@f09dc29f391db175b311fa44c00a2cb6a869d2a6 🚀

--- src.a5800543.js
+++ src.a5800543.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-15-iceCream/src.44f991d7.js.map
+//# sourceMappingURL=/project-15-iceCream/src.a5800543.js.map
